b0944250a4ad50a2bb1ced21006e8c9ed9425dee
Jessica Dene Earley-Cha <jearleycha@google.com>
2022-07-06 16:39:03 -0700

    add README

---
+++ README.md
@@ -0,0 +1,25 @@
+Affirmations App
+----------------
+
+
+**Description**
+
+An Andriod Application that displays Affirmations.
+
+Project sourced from: [Display a scrollable list](https://developer.android.com/courses/pathways/android-basics-kotlin-unit-2-pathway-3)
+
+
+### Technology Stack
+
+- Kotlin
+- RecyclerView
+
+### How to run Script locally
+
+This sample uses the Gradle build system. To build this project, use the "gradlew build" command or use "Import Project" in Android Studio.
+
+
+### About the Developer
+Jessica Dene Earley    
+[Bio](https://www.jessicadeneearley-cha.com/jessica)   
+[Linkedin](https://www.linkedin.com/in/jessicaearley)    
--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -3,9 +3,24 @@ package com.example.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize data and sets the list of affirmations
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // this setting is only needed to improve performance. Use this setting if you know
+        // that changes in content do not change the layout size of the RecyclerView.
+        recyclerView.setHasFixedSize(true)
+
     }
 }
